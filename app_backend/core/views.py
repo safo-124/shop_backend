d91bb90804a7b16f619de48d3432e7a3995436af
Safo Emmanuel <nanasafo122@gmail.com>
2024-08-25 21:20:30 +0800

    second commit

--- app_backend/core/views.py
+++ app_backend/core/views.py
@@ -35,10 +35,77 @@ class ProductList(generics.ListAPIView):
 
     def get_queryset(self):
         
-        queryset = models.Category.objects.all();
+        queryset = models.Product.objects.all();
         queryset = queryset.annotate(random_order=Count('id'))
 
         queryset = list(queryset)
         random.shuffle(queryset)
 
-        return queryset[:5]
+        return queryset[:20]
+    
+class PopularProductsList(generics.ListAPIView):
+    serializer_class = serializers.ProductSerializer
+
+    def get_queryset(self):
+        
+        queryset = models.Product.objects.filter(ratings__gte=4.0, ratings__lte=5.0);
+        queryset = queryset.annotate(random_order=Count('id'))
+
+        queryset = list(queryset)
+        random.shuffle(queryset)
+
+        return queryset[:20]
+    
+    
+class ProductListByClothesType(generics.ListAPIView):
+    serializer_class = serializers.ProductSerializer
+
+    def get_queryset(self, request):
+        query = request.query_params.get('clothesType', None)
+
+        if query:
+        
+            queryset = models.Product.objects.filter(clothesType=query);
+            queryset = queryset.annotate(random_order=Count('id'))
+
+            products_list = list(queryset)
+            random.shuffle(products_list)
+
+            limited_products = products_list[:20]
+
+            serializer = serializers.ProductSerializer(limited_products, many=True)
+
+            return Response(serializer.data)
+        
+        else:
+            return Response({'message': "No query provided"}, status=status.HTTP_400_BAD_REQUEST)
+        
+class SimilarProducts(APIView):
+    def get(self, request):
+        query = request.query_params.get('category', None)
+
+        if query:
+            products = models.Product.objects.filter(category = query);
+
+            product_list = list(products)
+            random.shuffle(product_list)
+
+            limited_products = product_list[:6]
+
+            serializer = serializers.ProductSerializer(limited_products, many=True)
+
+            return Response(serializer.data)
+        else:
+            return Response({'message': "No query provided"}, status=status.HTTP_400_BAD_REQUEST)
+        
+class SearchProductByTitle(APIView):
+    def get(self, request):
+        query = request.query_params.get('q', None)
+
+        if query:
+            products = models.Product.objects.filter(title_icontains=query);
+
+            serializer = serializers.ProductSerializer(products, many=True)
+            return Response(serializer.data)
+        else:
+            return Response({'message': "No query provided"}, status=status.HTTP_400_BAD_REQUEST)
